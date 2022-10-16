38f0a0608c725c692a72687a4b7086d62dce3701
Shreyas4895 <jshreyas11@gmail.com>
2022-10-16 08:33:36 +0530

    name change in sociallinks

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -19,7 +19,7 @@ const SocialLinks = () => {
       id: 2,
       child: (
         <>
-          LinkedIn <FaGithub size={30} />
+          GitHub <FaGithub size={30} />
         </>
       ),
       href: "https://github.com/Shreyas4895",
